f8fd71b7c54dec905ef789834f6bfb0139d817be
kobenguyent <7845001+kobenguyent@users.noreply.github.com>
2025-01-03 07:58:17 +0100

    fix: missing path import in Update AppiumV2_test.js

--- test/helper/AppiumV2_test.js
+++ test/helper/AppiumV2_test.js
@@ -2,6 +2,7 @@ const chai = require('chai')
 
 const expect = chai.expect
 const assert = chai.assert
+const path = require('path')
 
 let app
 const apk_path = 'storage:filename=selendroid-test-app-0.17.0.apk'
